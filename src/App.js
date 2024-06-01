3699ab58e10ec7e645e891768c3024f68acc52e7
redlixxt <119645715+redlixxt@users.noreply.github.com>
2024-05-31 22:04:52 -0700

    Learning prop
    HTML 태그에서 속성과 비슷한 특징을 가진 것을 의미한다.

--- src/App.js
+++ src/App.js
@@ -2,32 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 // 사용자 정의 태그라는 말을 쓰지 않고 컴포넌트라고 부른다.
 // 리액트의 본질 ? 코드가 단순해지며 이름을 가지고 있어서 어떤 취지인지 쉽게 파악할 수 있다. 동시에 수정할 수 있는 것도 특징적이다. 컴포넌트를 만드는 기술 덕분에 그들을 독립된 부품으로 만들 수 있게 됐고 그 부품을 이용하여 더 적은 복잡도로 소프트웨어를 만들 수 있게 됐다. 컴포넌트를 공유하여 생산성을 높일 수 있다는 장점도 있다.
-function Header() {
+function Header(props) {
+  console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">Web</a></h1>
+    <h1><a href="/">{props.title}</a></h1>
   </header>
 }
-function Nav() {
+function Nav(props) {
+  const lis = []
+  // 리액트가 태그를 추적해야 하는데 그 근거가 필요하다. 그 근거로 Key라고 하는 약속된 Prop을 부여함으로써 정확도를 높인다. 반복문 내에서 겹치지 않도록 부여한다.
+  for(let i=0; i<props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
   return <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">CSS</a></li>
-      <li><a href="/read/3">JavaScript</a></li>
+      {lis}
     </ol>
   </nav>
 }
-function Article() {
+function Article(props) {
   return <article>
-    <h2>Welcome</h2>
-    Hello, Web!
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
 function App() {
+  const topics = [
+    {id: 1, title: 'html', body: 'html is ...'},
+    {id: 2, title: 'CSS', body: 'CSS is ...'},
+    {id: 3, title: 'JavaScript', body: 'JavaScript is ...'}
+  ]
   return (
     <div>
-      <Header></Header>
-      <Nav></Nav>
-      <Article></Article>
+      <Header title="Web"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, Web"></Article>
     </div>
   );
 }
